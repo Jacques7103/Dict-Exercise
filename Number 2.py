5c419820738700421f11eea3cc1cf6aa7396124b
Jacques7103 <ferdinand.jacques@binus.ac.id>
2021-11-03 17:32:21 +0700

    Added Number 2

---
+++ Number 2.py
@@ -0,0 +1,12 @@
+my_dict = {
+    "John": ["BRV", "HRV", "CRV"],
+    "David": ["Civic", "CRZ", "Supra"],
+    "Wilson": ["Fortuner", "Pajero", "Terra"],
+    "Samuel": ["Mitsubishi 3000GT", "Honda S2000", "Nissan Skyline"],
+    "Willard": ["Mitsubishi Evo VI", "Honda NSX", "Mazda RX-7"]
+}
+
+for key, value in my_dict.items():
+    print(key,"likes these cars:")
+    for car in value:
+        print("- ", car)
